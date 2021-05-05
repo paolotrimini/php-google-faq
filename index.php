812c4ce5aa55f4a60590257b89f791762039fd90
paolotrimini <paolo.trimini@gmail.com>
2021-05-05 12:10:34 +0200

    caricamento file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+
+    <style>
+
+        .container {
+            background-color: darkgoldenrod;
+            color: white;
+        }
+
+    </style>
+
+    <title>php - Google FAQ</title>
+    <!-- bootstrap v4 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-3">
+
+                <?php
+                /*  Riscrivere questa pagina del sito google
+                    https://policies.google.com/faq.
+                    Ci sono diverse domande con relative risposte.
+                    Gestire il "Database" e la visualizzazione di
+                    queste domande e risposte con PHP.
+                */
+
+
+
+                ?>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
